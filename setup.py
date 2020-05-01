8146076988cdd25494bb9ba843693f378dff9d7f
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-05-01 14:27:33 -0700

    file with constants added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="noteline-sdk-core",
-    version="2.1.0",
+    version="2.2.0",
     author="Viacheslav Kovalevskyi",
     author_email="viacheslav@kovalevskyi.com",
     description="core Noteline SDK",
