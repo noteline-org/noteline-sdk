41943ec0dea1993a89a6ee5f60d67995a5fc320b
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-04-30 17:29:36 -0700

    core module introduced

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
-import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="noteline-sdk",
-    version="0.1.0",
+    version="1.1.1",
     author="Viacheslav Kovalevskyi",
     author_email="viacheslav@kovalevskyi.com",
     description="SDK for manipulating noteline specific metadata of the Notebooks",
